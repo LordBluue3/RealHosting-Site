36f3768e62ee77b826c4b5cc9f93d6569cba52d9
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-12-20 20:19:11 -0300

    Retirando a props size do componente paragrafo

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -14,11 +14,11 @@ export const Header = () => {
                 <div className='container_warns'>
                     <div>
                         <img src={discord} alt="Icone do discord" />
-                        <Paragraph color="white" size={14} text="Atendimento via Discord RealHosting"/>     
+                        <Paragraph color="white" text="Atendimento via Discord RealHosting"/>     
                     </div>
                     <div>
                         <img src={termos} alt="Termos e Políticas" />
-                        <Paragraph color="white" size={14} text="Termos e Políticas"/>
+                        <Paragraph color="white" text="Termos e Políticas"/>
                     </div>
                 </div>
             </div>
@@ -27,19 +27,19 @@ export const Header = () => {
                     <div>
                         <img src={hamburguer} alt="menu hamburguer responsivo" className='hamburguer' />
                         <img src={logo} alt="logo tipo da Real Hosting" className='logo' />
-                        <Paragraph color="orange" size={20} text="Real Hosting"/>
+                        <Paragraph color="orange" text="Real Hosting"/>
                     </div>
                     <ul>
-                        <li>Inicio</li>
+                        <li>Início</li>
                         <li>Hospedagem</li>
                         <li>Servidores</li>
                         <li>Suporte</li>
                     </ul>
                     <div className='container_nav-desk'>
-                        <Paragraph color="orange" size={14} text="Registre-se"/>
+                        <Paragraph color="orange" text="Registre-se"/>
                         <div className='container_button'>
                         <img src={user2} alt="área do cliente" className='icon_user2' />
-                        <Paragraph color="white" size={14} text="Minha conta"/>
+                        <Paragraph color="white" text="Minha conta"/>
                         </div>
                     </div>
                     <img src={user} alt="área do cliente" className='icon_user' />
